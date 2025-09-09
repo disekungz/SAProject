258413e097cc1375fc08a9b247c32c602a9798a3
disekungz <yozanakrub@gmail.com>
2025-09-09 17:10:07 +0700

    Update ranks.go

--- backend/entity/ranks.go
+++ backend/entity/ranks.go
@@ -3,7 +3,5 @@ package entity
 type Rank struct {
 	RankID   int    `gorm:"column:rank_id;primaryKey" json:"RankID"`
 	RankName string `gorm:"unique;not null" json:"RankName"`
-
-	Staff  []Staff  `gorm:"foreignKey:RankID"`
 	Member []Member `gorm:"foreignKey:RankID"`
 }
