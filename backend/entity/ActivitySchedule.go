01aad7e9a699500f8b637b05bbff4e7e4081c58f
chakkarinsuepsisuk-png <chakkarinsuepsisuk@gmail.com>
2025-09-09 19:40:48 +0700

    Update ActivitySchedule.go

--- backend/entity/ActivitySchedule.go
+++ backend/entity/ActivitySchedule.go
@@ -10,8 +10,11 @@ type ActivitySchedule struct {
 	EndTime     string    `gorm:"type:TIME" json:"EndTime"`
 	Max         int       `json:"max"`
 
-	MID    uint    `json:"mId"`
-	Member *Member `gorm:"foreignKey:MID;references:MID" json:"member"`
+	//MID    uint    `json:"mId"`
+	//Member *Member `gorm:"foreignKey:MID;references:MID" json:"member"`
+
+	StaffID *uint  `json:"staffId"`
+	Staff   *Staff `gorm:"foreignKey:StaffID;references:StaffID" json:"staff"`
 
 	Activity_ID uint      `json:"activity_ID"`
 	Activity    *Activity `gorm:"foreignKey:Activity_ID;references:Activity_ID" json:"activity"`
