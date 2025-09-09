e8aa11561802c8a9276987775b8e55b90b162988
Nawin01234 <1234witthawin@gmail.com>
2025-09-09 16:49:12 +0700

    Update axios.ts

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,10 +1,10 @@
+// src/lib/axios.ts
 import axios from "axios";
 import { getToken } from "./auth";
 
 export const api = axios.create({ baseURL: "http://localhost:8088/api" });
-
-api.interceptors.request.use((config) => {
-  const tk = getToken();
-  if (tk) config.headers.Authorization = `Bearer ${tk}`;
-  return config;
+api.interceptors.request.use(cfg => {
+  const t = getToken();
+  if (t) cfg.headers.Authorization = `Bearer ${t}`;
+  return cfg;
 });
