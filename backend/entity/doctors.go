31d2b2322f04dc7d191431816f5f4f7a18bb5a2d
disekungz <yozanakrub@gmail.com>
2025-09-09 20:17:47 +0700

    Delete backend/entity/doctors.go

--- backend/entity/doctors.go
+++
@@ -1,9 +0,0 @@
-package entity
-
-type Doctor struct {
-	DoctorID   int `gorm:"primaryKey"`
-	DoctorName string
-
-	// 1 PrisonID มี Jailer ได้หลาย
-	Medical_History []Medical_History `gorm:"foreignKey:DoctorID"`
-}
