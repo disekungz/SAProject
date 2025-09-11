5d6a1b2c244f68c08a134c5a9a8973b48df51f00
mangskm <mangtoy33@gmail.com>
2025-09-11 18:31:10 +0700

    Update Petition.go

--- backend/entity/Petition.go
+++ backend/entity/Petition.go
@@ -2,13 +2,18 @@ package entity
 
 import (
 	"time"
+
 	"gorm.io/gorm"
 )
 
+// Petition defines the structure for the petitions table
 type Petition struct {
 	gorm.Model
 	Detail       string
 	Date_created time.Time
+
+	Inmate_ID *uint
+	Inmate    Prisoner `gorm:"foreignKey:Inmate_ID"`
 
 	Staff_ID *uint
 	Staff    Staff `gorm:"foreignKey:Staff_ID"`
@@ -16,9 +21,13 @@ type Petition struct {
 	Status_ID *uint
 	Status    Status `gorm:"foreignKey:Status_ID"`
 
-	Type_cum_ID *uint // แก้ไขชื่อฟิลด์
-	Type    Type_cum `gorm:"foreignKey:Type_cum_ID"` // แก้ไข foreignKey ให้ตรงกับฟิลด์ที่ถูกต้อง
-
-	Inmate_ID *uint
-	Inmate    Prisoner `gorm:"foreignKey:Inmate_ID"`
+	Type_cum_ID *uint
+	Type        PetitionTypeCum `gorm:"foreignKey:Type_cum_ID"`
 }
+
+// PetitionTypeCum defines the structure for petition types
+type PetitionTypeCum struct {
+	gorm.Model
+	Type_cum_name string
+}
+
